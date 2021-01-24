48acb649ae94b34cf82d4f331110dd53580a5ac4
Matthew Pollicito <mpollicito@yahoo.com>
2021-01-24 14:26:16 -0500

    HTML updated

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Matthew Pollicito
-        </p>
-        <a
-          className="App-link"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Portfolio
-        </a>
-      </header>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#"><em>Matthew Pollicito</em></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Home/About Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="portfolio.html">Portfolio</a><span class="sr-only">(current)</span>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="contact.html">Contact Info</a>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
   );
 }
 
